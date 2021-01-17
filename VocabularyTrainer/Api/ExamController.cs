838a84d4b7d33bbff1477e8624e87efb1ff3505d
Georgi Yanakiev <georgi_87@hotmail.es>
2021-01-17 14:59:34 +0100

    Added Examp Web API

---
+++ VocabularyTrainer/Api/ExamController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VocabularyTrainer.Data;
+using VocabularyTrainer.Models;
+
+namespace VocabularyTrainer.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+        private readonly VocabularyTrainerContext _context;
+
+        public ExamController(VocabularyTrainerContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/exam/GetVocabularyExam/1
+        [HttpGet("{deckIds}")]
+        public IEnumerable<ExamWord> GetVocabularyExam(string deckIds)
+        {
+            var decks = deckIds.Split('|');
+            var words = _context.Word
+                .Where(w => decks.Contains(w.DeckId.ToString()))
+                .Select(w => new ExamWord
+                {
+                    Singular = w.Singular,
+                    Plural = w.Plural,
+                    Article = w.Article.ArticleName,
+                    Meaning = w.Meaning,
+                    CorrectAnswer = false
+                })
+                .ToList();
+
+            return words;
+        }
+    }
+}
---
+++ VocabularyTrainer/Models/ExamWord.cs
@@ -0,0 +1,11 @@
+﻿namespace VocabularyTrainer.Models
+{
+    public class ExamWord
+    {
+        public string Singular { get; set; }
+        public string Plural { get; set; }
+        public string Article { get; set; }
+        public string Meaning { get; set; }
+        public bool CorrectAnswer { get; set; }
+    }
+}
